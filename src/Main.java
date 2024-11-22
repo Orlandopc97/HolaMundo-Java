4f7e71ccfc68bc46a14ec112f6a14a8a1f494f06
Orlandopc97 <orlandopc1797@gmail.com>
2024-11-21 20:26:48 -0600

    Archivos hola mundo java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Función System.out.println
+        System.out.println("Hola Mundo :)");
+        System.out.println("Impresión por consola");
+        System.out.println("Atajo sout");
+
+    }
+}
